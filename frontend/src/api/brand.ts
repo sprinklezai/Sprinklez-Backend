59a6bd47716d783bf406636d999ac77b0e310a22
sprinklezai <sprinklezai@gmail.com>
2026-07-10 01:08:04 +0400

    Create backend repo

--- frontend/src/api/brand.ts
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export async function getBrandDashboard(brandCode: string) {
-  const response = await api.get(`/brand/${brandCode}`);
-  return response.data;
-}
--- frontend/src/api/overview.ts
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export async function getOverview() {
-  const response = await api.get("/overview");
-  return response.data;
-}
